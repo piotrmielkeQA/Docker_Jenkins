68f2875f81cfb23d8e40ad38fedc1dfecc325566
piotrek <piotrek.mielke@gmail.com>
2021-06-11 13:52:50 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace altkomPiotrMielke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.WriteLine("Hi Pawel");
+            for(int i=0;i<10;i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine(rand.Next(100,999));
+            }
+        }
+    }
+}
